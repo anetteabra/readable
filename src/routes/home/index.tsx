332fcf99a63bbbc1241c1d1a13a21fbc96d8787d
Frida Benedikte Iversen <fridabi@stud.ntnu.no>
2024-09-30 16:24:04 +0200

    added search bar, without any function

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,12 +1,10 @@
 import styles from './Home.module.css';
-import { Button } from "@/components/ui/button";
 
 const Home = () => {
 
   return (
     <>
       <div className={styles.controls}>
-       <Button>Click me</Button>
       </div>
     </>
   );
