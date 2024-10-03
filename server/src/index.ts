8b93b6101f71739996e6235642548b85819d3fe7
Frida Benedikte Iversen <fridabi@stud.ntnu.no>
2024-10-03 12:58:36 +0200

    fixed a server map and connction to the apollo server

---
+++ server/src/index.ts
@@ -0,0 +1,47 @@
+/* const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const { addMocksToSchema } = require('@graphql-tools/mock');
+const { makeExecutableSchema } = require('@graphql-tools/schema'); */
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { addMocksToSchema } from '@graphql-tools/mock';
+import typeDefs from './schema';
+
+
+/* const typeDefs = require('./schema');  */
+
+const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(6)],
+  }),
+  Book: () => ({
+    id: () => 'track_01',
+    title: () => 'Astro Kitty, Space Explorer',
+    author: () => {
+      return {
+        name: 'Grumpy Cat',
+        photo: 'https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg',
+      };
+    },
+    thumbnail: () => 'https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg',
+    length: () => 1210,
+    modulesCount: () => 6,
+  }),
+};
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    schema: addMocksToSchema({
+      schema: makeExecutableSchema({ typeDefs }),
+      mocks,
+    }),
+  });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+      🚀  Server is running
+      📭  Query at ${url}
+    `);
+}
+
+startApolloServer();
