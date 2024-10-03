4f05758daf41106bcf2ce1e43919af3e9bff8a58
Mari Stampeløkken <marstamp@stud.ntnu.no>
2024-10-03 12:52:55 +0200

    feat: library button
    add button that links to library
    
    issue #8

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,4 +1,6 @@
 import styles from './Home.module.css';
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
 
@@ -7,6 +9,11 @@ const Home = () => {
       <div className={styles.controls}>
         <h1 className={styles.header}> Explore new worlds, one book at a time</h1>
         <p className={styles.paragraph}>Join the online bookclub</p>
+        
+        <Link to="/new">
+          <Button className={styles.button}>Go to your library</Button>
+        </Link>
+      
       </div>
     </>
   );
