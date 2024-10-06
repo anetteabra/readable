75fca3f87db154012112a37c43741b0549672e87
liselun <liselun@ntnu.no>
2024-10-06 17:56:38 +0200

    style: remove a detail
    remove detail because it does not fit in, did not see it in last commit
    
    issue: #11, #9

--- src/components/Loading/LoadingButton.tsx
+++ src/components/Loading/LoadingButton.tsx
@@ -1,10 +1,9 @@
-import { Loader2 } from "lucide-react";
+
 import { Button } from "../ui/button";
 import styles from "./LoadingButton.module.css";
 
 export default function (){
     return(
-        <><Loader2 className="mr-2 h-4 w-4 animate-spin" />
-        <Button className={styles.LoadingButton}>Load more</Button></>
+        <Button className={styles.LoadingButton}>Load more</Button>
     );
 };
