12cac03f73b352371e34a2289cbfc3aa0e5db9b3
Anette Abrahamsen <anettsae@stud.ntnu.no>
2024-10-07 11:24:04 +0200

    feat: VM setup
    VM setup completed, but project doesn't load on the homepage
    
    Issue: #20

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/project2',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
