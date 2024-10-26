8997e507101faa17333f1e9537050a92f51021f3
liselun <liselun@ntnu.no>
2024-10-26 12:19:02 +0200

    style: reviewlist updated
    smaller component, message if no review
    
    issue: #51

--- src/components/ReviewList/index.tsx
+++ src/components/ReviewList/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
 import Comment from "../ReviewComment";
@@ -6,63 +5,39 @@ import styles from "./ReviewList.module.css";
 import { GET_REVIEWS, Review, ReviewsProps } from "@/queries";
 import { useQuery } from "@apollo/client";
 
-/* const reviews = [
-  {
-    name: "John Doe",
-    stars: 5,
-    comment: "Excellent book, highly recommended!",
-  },
-  {
-    name: "Jane Smith",
-    stars: 4,
-    comment: "Very insightful, but some chapters were too long.",
-  },
-  {
-    name: "Mark Wilson",
-    stars: 3,
-    comment: "Not as engaging as I hoped.",
-  },
-  {
-    name: "Alice Johnson",
-    stars: 4,
-    comment: "Good read, but could be shorter.",
-  },
-  {
-    name: "Bob Brown",
-    stars: 5,
-    comment: "A must-read for anyone interested in the subject!",
-  },
-]; */
-
 const ReviewList: React.FC<ReviewsProps> = ({ bookId }) => {
-  const { loading, error, data } = useQuery<{ reviews: Review[] }>(
-    GET_REVIEWS,
-    {
-      variables: { bookId },
-    },
-  );
+  const { loading, error, data } = useQuery<{ reviews: Review[] }>(GET_REVIEWS, {
+    variables: { bookId },
+  });
 
   if (loading) return <p>Loading reviews...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+  // If data is defined, check for reviews
+  const reviews = data?.reviews;
+
   return (
-    <>
-      <ScrollArea className={styles.scrollArea}>
-        <h1 className={styles.title}>Reviews of this book</h1>
-        {data?.reviews.map((review, index) => (
+    <ScrollArea className={styles.scrollArea}>
+      <h1 className={styles.title}>Reviews of this book</h1>
+
+      {/* Check if there are any reviews */}
+      {reviews?.length === 0 ? (
+        <p>No reviews of this book yet</p>
+      ) : (
+        reviews?.map((review, index) => (
           <div key={index}>
             <Comment
               name={review.name}
               stars={review.stars}
               comment={review.comment}
             />
-            {index < data.reviews.length - 1 && (
+            {index < reviews.length - 1 && (
               <Separator className={styles.separator} />
             )}
           </div>
-        ))}
-      </ScrollArea>
-    </>
+        ))
+      )}
+    </ScrollArea>
   );
 };
 
