d5dd52e18d26b82142aab6b0c32407f9f235d35d
Anette Abrahamsen <anettsae@stud.ntnu.no>
2024-11-05 13:36:08 +0100

    feat: mutation addReview and server config
    In schema I have added a mutation such that addReview works, in addition Ive added script compile in server/package.json and Ive made a tsconfig file in the server folder. Which is important for running the backend on VM
    
    Issue: #61

--- server/src/schema.ts
+++ server/src/schema.ts
@@ -4,7 +4,7 @@ import { gql } from "graphql-tag";
 const typeDefs = gql`
 
   type Book {
-    id: ID! # Unique identifier for the book
+    id: ID! @id @unique
     title: String! # Title of the book
     author: Author! @relationship(type: "WROTE", direction: IN) # The author of the book
     cover: String # URL of the book's cover image
@@ -12,34 +12,46 @@ const typeDefs = gql`
     genre: String
     publication_date: String
     isbn13: String
+    reviews: [Review!]! @relationship(type: "REVIEWED", direction: IN)
   }
 
   type Author {
-    id: ID! # Unique identifier for the author
+    id: ID! @id @unique
     name: String! # Name of the author
     books: [Book!]! @relationship(type: "WROTE", direction: OUT) # Books written by the author
   }
 
   type Review {
-    id: ID!
+    id: ID! @id @unique
     name: String!
     stars: Int!
     comment: String!
     book: Book! @relationship(type: "REVIEWED", direction: OUT)
   }
-
+  
+  type TestSubscriber {
+    id: ID! @id @unique
+    name: String!
+    email: String!
+    Book: Book! @relationship(type: "SUBSCRIBED", direction: OUT)
+  }
+  
   type Mutation {
-    addBook(
-      title: String!,
-      cover: String,
-      authorName: String!,
-      description: String,
-      genre: String,
-      publication_date: String,
-      isbn13: String
-    ): Book
-
-    addReview(bookId: ID!, name: String!, stars: Int!, comment: String!): Review
+    addReview(
+      bookId: ID!,
+      name: String!,
+      stars: Int!,
+      comment: String!
+    ): Review!
+    @cypher(
+      statement: """
+        MATCH (b:Book {id: $bookId})
+        CREATE (r:Review {name: $name, stars: $stars, comment: $comment})
+        CREATE (r)-[:REVIEWS]->(b)
+        RETURN r
+      """,
+      columnName: "r"
+      )
   }
 `;
 
