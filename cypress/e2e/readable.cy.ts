6778128405398fffcc7b3d0d70050926b580fa49
Anette Abrahamsen <anettsae@stud.ntnu.no>
2024-11-13 12:16:56 +0100

    feat: e2e test
    Added simple e2e test called readable.cy.ts. To not get CSRF error I had to add headers to apolloClient.ts. In addition I added data-cy to button on home page.
    
    Issue: #88

---
+++ cypress/e2e/readable.cy.ts
@@ -0,0 +1,20 @@
+/// <reference types="cypress" />
+
+describe('E2E Tests for Homepage', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('should visit the homepage', () => {
+    // Already visited in beforeEach, no need to visit again.
+  });
+
+  it('should display the homepage title', () => {
+    cy.contains('Explore new worlds, one book at a time').should('be.visible');
+  });
+
+  it('should navigate to the library page on button click', () => {
+    cy.get('[data-cy="library-button"]').click();
+    cy.url().should('include', '/library');
+  });
+});
--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,7 +1,14 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3001",
+  link: new HttpLink({
+    uri: "http://localhost:3001", // Ensure this points to your GraphQL endpoint
+    headers: {
+      "Content-Type": "application/json", // Specify the content type
+      "x-apollo-operation-name": "default", // Optional: Set a default operation name
+      "apollo-require-preflight": "true",  // Optional: Helps bypass CSRF checks
+    },
+  }),
   cache: new InMemoryCache(),
 });
 
